e105b79ee08be3525f4a6f4033983b5afb94f1a8
MichalPiotrowski0786 <michal.piotrowski.g@gmail.com>
2021-10-25 23:46:10 +0200

    Done

--- PiramidaFinansowa/Program.cs
+++ PiramidaFinansowa/Program.cs
@@ -4,8 +4,8 @@ using System.IO;
 using System.Xml;
 
 namespace PiramidaFinansowa {
-    class Program {
 
+    public class Program {
         static readonly string basePath = Path.GetFullPath(@"..\..\..\Input");
         static readonly string piramidFilePath = $@"{basePath}\piramida.xml";
         static readonly string transfersFilePath = $@"{basePath}\przelewy.xml";
@@ -14,10 +14,10 @@ namespace PiramidaFinansowa {
             var piramidDoc = ReadXmlFile(piramidFilePath);
             var transfersDoc = ReadXmlFile(transfersFilePath);
 
-            if(piramidDoc != null || transfersDoc != null) ResolvePiramidHierarchy(piramidDoc,transfersDoc);
+            if(piramidDoc != null || transfersDoc != null) Compute(piramidDoc,transfersDoc);
         }
 
-        static void ResolvePiramidHierarchy(XmlDocument baseDoc, XmlDocument transfersDoc) {
+        static void Compute(XmlDocument baseDoc, XmlDocument transfersDoc) {
             var mainNode = baseDoc.GetElementsByTagName("uczestnik");
             List<Member> members = new();
 
@@ -30,15 +30,43 @@ namespace PiramidaFinansowa {
                 int childsIndex = 0;
                 int childs = CalculateChildsWithNoChilds(childsIndex, Node);
 
+                int parentId = CalculateUserParent(Node);
                 uint transferSum = ReturnSumOfMemberTransfers(id, transfersDoc);
 
-                members.Add(new(id, position, childs, transferSum));
+                members.Add(new(id, parentId, position, childs, transferSum));
             }
 
             if(members.Count > 0) {
+                Member owner = null;
+                foreach(Member member in members) {
+                    if(member.position == 0) owner = member;
+                }
+
+                foreach(Member member in members) {
+                    member.transferMoney(member.balance, owner);
+                }
+
+                foreach(Member member in members) {
+                    if(member.childs > 0) {
+                        List<Member> childs = new();
+                        foreach(Member _member in members) {
+                            if(_member.parentId == member.id) childs.Add(_member);
+                        }
+
+                        if(childs.Count > 0) {
+                            foreach(Member child in childs) {
+                                owner.transferMoney(child.init_balance / 2, member);
+                            }
+                        }
+                    }
+                }
+
                 foreach(Member member in members) {
                     Console.WriteLine(
-                        $"{member.id} {member.position} {member.childs} {member.transfersSum}"
+                        $"{member.id} " +
+                        $"{member.position} " +
+                        $"{member.childs} " +
+                        $"{member.balance} "
                     );
                 }
             }
@@ -53,6 +81,14 @@ namespace PiramidaFinansowa {
             }
 
             return index;
+        }
+
+        static int CalculateUserParent(XmlNode node) {
+            int parentId;
+            if(node.ParentNode == null || node.ParentNode.Name == "piramida") parentId = -1;
+            else parentId = int.Parse(node.ParentNode.Attributes["id"].Value);
+
+            return parentId;
         }
 
         static int CalculateChildsWithNoChilds(int index, XmlNode node) {
@@ -81,6 +117,7 @@ namespace PiramidaFinansowa {
         }
 
         static XmlDocument ReadXmlFile(string path) {
+            // load XML document
             if(!IsInputValid(path)) return null;
             XmlDocument doc = new ();
             doc.Load(path);
@@ -89,6 +126,7 @@ namespace PiramidaFinansowa {
         }
 
         static bool IsInputValid(string path) {
+            // check if file exists
             bool inputValid = true;
             if(!File.Exists(path)) inputValid = false;
             return inputValid;
@@ -98,15 +136,27 @@ namespace PiramidaFinansowa {
     public class Member {
         // using class to better organize individual members and actions related to them
         public int id { get; set; }
+        public int parentId { get; set; }
         public int position { get; set; }
         public int childs { get; set; }
-        public uint transfersSum { get; set; }
+        public uint childsBalance { get; set; }
+        public uint init_balance { get; set; }
+        public uint balance { get; set; }
 
-        public Member(int id, int position, int childs, uint transfersSum) {
+        public Member(int id, int parentId, int position, int childs, uint balance) {
             this.id = id;
+            this.parentId = parentId;
             this.position = position;
             this.childs = childs;
-            this.transfersSum = transfersSum;
+            init_balance = balance;
+            this.balance = balance;
+        }
+
+        public void transferMoney(uint amount, Member member) {
+            if(balance > 0 && amount <= balance) {
+                member.balance += amount;
+                balance -= amount;
+            }
         }
     }
 }
